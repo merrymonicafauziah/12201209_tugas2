3f8882fa1430a712d67bd78c1f6b6835b407346d
merrymonicafauziah <merrymonica60@gmail.com>
2021-09-25 19:30:29 +0700

    menambahkan tugas 2 dan biodata

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Merry Monica Fauziah
+			  Saya tinggal di Mangkubumi
+			  No HP 089636616010";
+	}
+}
